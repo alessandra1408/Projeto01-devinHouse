2d18adc169d02f3ce680ca52c6e1239141fe35c6
alessandra1408 <alessandraoitnb@gmail.com>
2021-07-23 22:11:43 -0300

    Mudando estrutura dos li e botões. Adicionando div.

--- projeto01.js
+++ projeto01.js
@@ -1,19 +1,14 @@
 
 var form = document.querySelector("#form");
-/* var btnAdd = document.querySelector("#btnAdd");
- */var inputItem = document.getElementById("inputTxt");
+var inputItem = document.getElementById("inputTxt");
 var lista = document.querySelector("#lista");
 var btnExclui = document.getElementById("btnExclui");
 var divBotoes = document.querySelector("#divBotoes");
+var divBtnLi;
 var novoBtnFeito;
-var novoItem;
+var li;
 var novoBtnExcluir;
-
-
 var itensDaLista = [];
-
-/* btnAdd.addEventListener("click", adicionarLocal);
- */
 
 btnExclui.addEventListener("click", function (event) {
     excluiLista();
@@ -22,62 +17,62 @@ btnExclui.addEventListener("click", function (event) {
 form.addEventListener("submit", function (event) {
     event.preventDefault();
     salvarLista(inputItem.value);
-    /* criarBotoes(); */
 });
-
 
 mostraLista();
 
-/* function adicionarLocal() {
-    //TODO: Adicionar item no localStorage
-    console.log("Entrou na Local");
-} */
-
 function salvarLista(txtItem) {
-    var optDefault = document.getElementById("optDefault");
     if (txtItem) {
-        if (optDefault) {
-            lista.remove(optDefault);
-            console.log("Removeu o 1o");
-        }
-        console.log("Entrou na Lista");
-        novoItem = document.createElement("option");
-        novoItem.innerHTML = txtItem;
-        novoItem.value = txtItem;
-        lista.appendChild(novoItem);
+
+        divBtnLi = document.createElement("div");
+        divBtnLi.classList.add("divBtnLi");
+        divBtnLi.innerHTML = `
+        <input type="checkbox" >
+        <li>${txtItem}</li>
+        <input type="button" value="X">
+        `
+        lista.appendChild(divBtnLi);
+
+        /* li = document.createElement("li");
+        li.appendChild(document.createTextNode(txtItem));
+        lista.appendChild(li);
+        divBtnLi.appendChild(botaoFeito());
+        divBtnLi.appendChild(botaoExclui()); */
+
         itensDaLista.push(txtItem);
         inputItem.value = "";
         inputItem.focus();
-        criarBotoes();
 
         localStorage.setItem("lista", JSON.stringify(itensDaLista))
 
-        console.log("leu tudo");
-
-        return novoItem;
+        return li;
     }
     else {
-        console.log("Favor inserir um item")
+        alert("Favor inserir um item")
     }
     console.log(txtItem);
-
 }
 
-function criarBotoes() {
+
+
+function botaoFeito() {
 
     novoBtnFeito = document.createElement("button");
     novoBtnFeito.innerHTML = "✅";
     novoBtnFeito.value = "✅";
-    novoItem.setAttribute("id", "btnFeito")
-    novoItem.classList.add("optNormal");
-    divBotoes.appendChild(novoBtnFeito);
+    /* li.setAttribute("id", "btnFeito")
+    li.classList.add("optNormal"); */
+
+    return novoBtnFeito;
+}
+
+function botaoExclui() {
 
     novoBtnExcluir = document.createElement("button");
     novoBtnExcluir.innerHTML = "❌";
     novoBtnExcluir.value = "❌";
-    divBotoes.appendChild(novoBtnExcluir);
 
-    return novoBtnFeito;
+    return novoBtnExcluir;
 }
 
 function mostraLista() {
@@ -92,10 +87,10 @@ function mostraLista() {
 }
 
 function efeitosBotoes(botao) {
-        novoItem.classList.remove("optNormal");
-        novoItem.classList.add("optRiscado");
-    }
+        li.classList.remove("optNormal");
+        li.classList.add("optRiscado");
+}
 
 function excluiLista() {
-    localStorage.clear();
+    localStorage.removeItem(divBtnLi);
 }
